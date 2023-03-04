08c7907fd62394964279ac57abd64f9f0b517b20
ans-naeem <ans.naeem31@gmail.com>
2023-03-04 18:00:51 +0500

    station analysis done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session, sessionmaker
-from sqlalchemy import create_engine, MetaData,func
+from sqlalchemy import create_engine, MetaData, func, desc
 from sqlalchemy.ext.declarative import declarative_base
 import csv
 from datetime import datetime, timedelta
 import pandas as pd
+import matplotlib.pyplot as plt
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
@@ -37,9 +38,9 @@ for temp_stations in stations:
 # for temp_measurement in measure:
 #     print(temp_measurement.date, temp_measurement.prcp)
 
-#-------------------------------------------
+# ************************ ************************
 #Now the Precipitation Analysis:
-#-------------------------------------------
+# ************************ ************************
 
 
 #getting most recent date from dataset
@@ -63,7 +64,7 @@ for temp_measurement in measure:
         most_recent_date = date_str
 print(most_recent_date.strftime('%Y-%m-%d'))
 
-#second way:
+#************ second way *****************:
 most_recent_date2 = session.query(measurment.date).order_by(measurment.date.desc()).first()[0]
 print(most_recent_date2)
 
@@ -75,7 +76,9 @@ previous_year_results = session.query(measurment.date, measurment.prcp).filter(m
 for date, prcp in previous_year_results:
     print(f"Date: {date}, Precipitation: {prcp}")
 
-#*****************its for filling ************************
+# ************************ ************************
+# its for file reading
+# ************************ ************************
 # Open the file for reading
 # with open('Resources/hawaii_measurements.csv', 'r') as f:
 #     reader = csv.reader(f)
@@ -103,3 +106,82 @@ for date, prcp in previous_year_results:
 #             most_recent_date = date_str
 # print(most_recent_date.strftime('%Y-%m-%d'))
 
+
+
+#****************************************************************
+# now using pandas:
+#****************************************************************
+
+# Load the query results into a pandas DataFrame
+df = pd.DataFrame(previous_year_results, columns=['date', 'precipitation'])
+
+# Set the index to the "date" column
+df.set_index('date', inplace=True)
+
+
+#using the DataFrame plot method
+# df_sorted = df.sort_values('date')
+#
+# df_sorted.plot()
+# plt.show()
+
+#using Pandas to print the summary statistics for the precipitation data.
+precipitation_stats = df['precipitation'].describe()
+print(precipitation_stats)
+
+
+# ************************ ************************
+#Now the station Analysis:
+# ************************ ************************
+
+#Query to count the number of rows in the "station" table
+station_count = session.query(func.count(station.id)).scalar()
+print(station_count)
+
+#Query for counting the observation count for every station.
+observation_count_results = session.query(measurment.station, func.count(measurment.station),measurment.date).\
+            group_by(measurment.station).\
+            order_by(desc(func.count(measurment.station))).all()
+
+# for one_row in observation_count_results:
+#     print(one_row)
+
+station_id_with_most_observations =observation_count_results[0][0]
+station_with_most_observations_count =observation_count_results[0][1] #because it is already sorted in desc order
+
+statistics_of_most_observation = session.query(func.min(measurment.tobs), func.max(measurment.tobs), func.avg(measurment.tobs)) \
+                 .filter(measurment.station == station_id_with_most_observations) \
+                 .all()
+
+print(f"Lowest Temperature: {statistics_of_most_observation[0][0]}")
+print(f"Highest Temperature: {statistics_of_most_observation[0][1]}")
+print(f"Average Temperature: {statistics_of_most_observation[0][2]}")
+
+
+
+date_of_having_most_observation_station =observation_count_results[0][2]
+print (date_of_having_most_observation_station)
+last_year_date = datetime.strptime(date_of_having_most_observation_station, '%Y-%m-%d') - timedelta(days=365)
+previous_year_results_for_tob = session.query(measurment.station, measurment.tobs).filter(measurment.date >= last_year_date)\
+                  .filter(measurment.date <= date_of_having_most_observation_station)\
+                  .all()
+# for station, tobs in previous_year_results_for_tob:
+#     print(f"station: {station}, TOBS: {tobs}")
+
+
+temperatures = []
+for row in previous_year_results_for_tob:
+    temperatures.append(row[1])
+
+# Plot the histogram
+plt.hist(temperatures, bins=12)
+plt.xlabel("Temperature ")
+plt.yticks(range(0, 10, 1))
+plt.xticks(range(50,85,5))
+plt.ylabel("Frequency")
+plt.title(f"Temperature Observations for Station")
+plt.show()
+
+
+#closing the session:
+session.close()
